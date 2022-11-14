fff7bf52b73ff3a4aaf47f4a460424eb96440d16
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2022-11-14 13:49:09 -0600

    Added total cheese count to About page. 🧀

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,8 +1,20 @@
+import { useSelector, useDispatch } from 'react-redux';
+
 export default function About() {
+  const count = useSelector((state: any) => state.cheeseCounter.value);
+
+  let cheeseCountMessageElement = (
+    count > 0 ?
+      <p>You have cheesed {count} time{count > 1 ? 's' : ''}.</p>
+      :
+      <p>You have not cheesed yet. Sadge</p>
+  );
+
   return (
     <div>
       <h1>About</h1>
       <p>Welcome to <code>Josh's Cool App</code> which was made by me (Josh), and it is pretty cool if I do say so myself.</p>
+      {cheeseCountMessageElement}
       <p>Copyright &copy; { (new Date()).getFullYear() }&mdash;{ (new Date()).getFullYear() + 1234 } Josh. All rights reserved.</p>
     </div>
   );
